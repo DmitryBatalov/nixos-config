c80265530d13b4499e21216192d48bff05c5cd3b
dmitry.batalov <dmtiryabat@gmail.com>
2025-10-21 07:16:47 +0200

    chore(cfg): add btop, replace alacritty with st

--- configuration.nix
+++ configuration.nix
@@ -82,6 +82,8 @@
     packages = with pkgs; [
       tree
       docker-compose
+      btop
+      lazygit
     ];
   };
 
@@ -92,9 +94,8 @@
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     wget
-    alacritty
+    st
     git
-    lazygit
   ];
 
   fonts.packages = with pkgs; [
