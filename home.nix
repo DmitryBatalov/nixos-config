1ae1da3aa1b91ccced2553ce4720db85ca2fcc0e
dmitry.batalov <dmtiryabat@gmail.com>
2025-10-25 09:25:08 +0200

    style: format home.nix

--- home.nix
+++ home.nix
@@ -1,29 +1,34 @@
-{ config, pkgs, ... }: 
-
 {
-	home.username = "dmitry";
-	home.homeDirectory = "/home/dmitry";
+  config,
+  pkgs,
+  ...
+}: {
+  home = {
+    username = "dmitry";
+    homeDirectory = "/home/dmitry";
+  };
 
-	programs = {
-		git = {
-			enable = true;
-			userName = "dmitry.batalov";
-			userEmail = "dmtiryabat@gmail.com";
-			extraConfig = {
-				init.defaultBranch = "main";
-			};
-		};
-		lazygit.enable = true;
-	};
+  programs = {
+    git = {
+      enable = true;
+      userName = "dmitry.batalov";
+      userEmail = "dmtiryabat@gmail.com";
+      extraConfig = {
+        init.defaultBranch = "main";
+      };
+    };
+    lazygit.enable = true;
+    neovim.enable = true;
+  };
 
-	home.stateVersion = "25.05";
-	programs.bash = {
-		enable = true;
-		shellAliases = {
-			lg = "lazygit";
-		};
-	};
-	
+  home.stateVersion = "25.05";
 
-	# home.file.".config/i3".source = ./config/i3;
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      lg = "lazygit";
+    };
+  };
+
+  #home.file.".config/i3".source = ./config/i3;
 }
