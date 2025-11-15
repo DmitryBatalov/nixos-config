88507f49e3f293d1c4b36682341ee4c05aa2ac00
dmitry.batalov <dmtiryabat@gmail.com>
2025-11-15 19:00:26 +0100

    chore(modules/i3): change config according to warning.

--- modules/i3.nix
+++ modules/i3.nix
@@ -2,6 +2,7 @@
   # i3 related options
   environment.pathsToLink = ["/libexec"]; # links /libexec from derivations to /run/current-system/sw
 
+  services.displayManager.defaultSession = "none+i3";
   services.xserver = {
     enable = true;
 
@@ -15,7 +16,6 @@
     displayManager = {
       lightdm.enable = false;
       gdm.enable = true;
-      defaultSession = "none+i3";
     };
 
     windowManager.i3 = {
