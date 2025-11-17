5d6de646c53e0ae1de4fb7ef86e598f2a70cf289
dmitry.batalov <dmtiryabat@gmail.com>
2025-11-17 07:41:20 +0400

    chore(i3/config): change i3 config

--- home/i3/default.nix
+++ home/i3/default.nix
@@ -8,7 +8,7 @@
     file = {
       ".config/i3/config".source = ./config;
       ".config/i3/i3blocks.conf".source = ./i3blocks.conf;
-      ".config/i3/keybindings".source = ./keybindings;
+      # ".config/i3/keybindings".source = ./keybindings;
       ".config/i3/scripts" = {
         source = ./scripts;
         # copy the scripts directory recursively
