70294c706a062aa53f4d29137bbfa898249468a5
dmitry.batalov <dmtiryabat@gmail.com>
2025-11-17 07:42:19 +0400

    chore(docker): add address pool to docker deamon settings

--- modules/docker.nix
+++ modules/docker.nix
@@ -6,6 +6,15 @@
   virtualisation.docker = {
     enable = true;
     enableOnBoot = true;
+    # setup address pool
+    daemon.settings = {
+      default-address-pools = [
+        {
+          base = "10.10.0.0/16";
+          size = 24;
+        }
+      ];
+    };
   };
 
   users.users.${username}.extraGroups = ["docker"];
