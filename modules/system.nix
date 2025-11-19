db31f842337614b3be21e28e2d85259ff64c5041
dmitry.batalov <dmtiryabat@gmail.com>
2025-11-19 22:01:05 +0400

    style(system): code cleanup

--- modules/system.nix
+++ modules/system.nix
@@ -129,16 +129,12 @@
 
     dbus.packages = [pkgs.gcr];
 
-    #geoclue2.enable = true;
-
     blueman.enable = true;
     pipewire = {
       enable = true;
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      # jack.enable = true;
 
       extraConfig.pipewire."92-low-latency" = {
         context.properties = {
