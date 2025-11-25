5e6ce6920d98283e96cb80c1e46a3fe518839f25
dmitry.batalov <dmtiryabat@gmail.com>
2025-11-25 14:06:04 +0400

    chore(home): add freecad

--- users/dmitry/home.nix
+++ users/dmitry/home.nix
@@ -23,6 +23,7 @@ in {
     pkgs.libreoffice-qt6-fresh
     unstable.jetbrains.rider
     unstable.freelens-bin
+    pkgs.freecad
     (pkgs.google-cloud-sdk.withExtraComponents [pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin])
     # pkgs.google-cloud-sdk
     pkgs.kubectl
