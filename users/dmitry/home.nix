4d8d6034cfc9a89fca4ad23856624e9066eff8f2
dmitry.batalov <dmtiryabat@gmail.com>
2025-11-25 14:06:15 +0400

    chore(home): add obsidian

--- users/dmitry/home.nix
+++ users/dmitry/home.nix
@@ -24,6 +24,7 @@ in {
     unstable.jetbrains.rider
     unstable.freelens-bin
     pkgs.freecad
+    pkgs.obsidian
     (pkgs.google-cloud-sdk.withExtraComponents [pkgs.google-cloud-sdk.components.gke-gcloud-auth-plugin])
     # pkgs.google-cloud-sdk
     pkgs.kubectl
