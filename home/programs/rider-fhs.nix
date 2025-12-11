2e37568b7b4dc62e36a1d552f1b5f0fd59823825
dmitry.batalov <dmtiryabat@gmail.com>
2025-12-11 10:16:35 +0400

    chore(rider): add dotnet 6 & 10 to FHS rider

--- home/programs/rider-fhs.nix
+++ home/programs/rider-fhs.nix
@@ -1,24 +1,26 @@
-{pkgs ? import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-25.11.tar.gz") {}}:
-(pkgs.buildFHSEnv {
-  name = "rider-dotnet";
+{pkgs ? import <nixpkgs> {}}:
+(
+  let
+    pkgsWithInsecure = import <nixpkgs> {
+      config = {
+        permittedInsecurePackages = [
+          # "dotnet-sdk_6"
+          "dotnet-sdk-6.0.428"
+        ];
+      };
+    };
+    dotnet = with pkgs.dotnetCorePackages; combinePackages [dotnet_10.sdk dotnet_8.sdk pkgsWithInsecure.dotnet-sdk_6];
+  in
+    pkgs.buildFHSEnv {
+      name = "rider-dotnet";
+      profile = ''
+        export DOTNET_ROOT=${dotnet}/share/dotnet
+        export DOTNET_PATH=${dotnet}/bin/dotnet
+        export PATH=$PATH:${dotnet}/share/dotnet
+      '';
 
-  targetPkgs = pkgs:
-    with pkgs; [
-      dotnetCorePackages.sdk_8_0-bin
-
-      # Essential libraries
-      icu
-      openssl
-      zlib
-      stdenv.cc.cc.lib
-    ];
-
-  profile = ''
-    export DOTNET_ROOT=${pkgs.dotnet-sdk_8}
-    export MSBuildSDKsPath=${pkgs.dotnet-sdk_8}/lib/sdk/8.0.100/Sdks
-  '';
-
-  runScript = ''
-    nohup rider > /dev/null 2>&1 &
-  '';
-}).env
+      runScript = ''
+        nohup rider > /dev/null 2>&1 &
+      '';
+    }
+).env
