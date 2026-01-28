d6c2b2ea77353283d5489ea802c504b4d1524087
dmitry.batalov <dmtiryabat@gmail.com>
2026-01-28 12:29:37 +0400

    chore(home): make rider un-proxied

--- home/programs/rider-fhs.nix
+++ home/programs/rider-fhs.nix
@@ -12,21 +12,12 @@ let
   dotnet = with pkgs.dotnetCorePackages;
     combinePackages [ dotnet_10.sdk dotnet_8.sdk pkgsWithInsecure.dotnet-sdk_6 ];
 
-  proxychainsConf = pkgs.writeText "proxychains.conf" ''
-    strict_chain
-    quiet_mode
-    proxy_dns
-
-    [ProxyList]
-    socks5 127.0.0.1 1081
-  '';
 in
 pkgs.buildFHSEnv {
   name = "rider";
 
   targetPkgs = _: [
     unstable.jetbrains.rider
-    pkgs.proxychains-ng
     dotnet
   ];
 
@@ -34,8 +25,7 @@ pkgs.buildFHSEnv {
     export DOTNET_ROOT=${dotnet}/share/dotnet
     export DOTNET_PATH=${dotnet}/bin/dotnet
     export PATH=$PATH:${dotnet}/share/dotnet
-    export PROXYCHAINS_CONF_FILE=${proxychainsConf}
   '';
 
-  runScript = "proxychains4 rider";
+  runScript = "rider";
 }
