7f6a90902d70e163e40ddc59b450f5dc60a421d7
dmitry.batalov <dmtiryabat@gmail.com>
2026-02-27 09:41:19 +0400

    chore: add k9s

--- home/kube/default.nix
+++ home/kube/default.nix
@@ -12,6 +12,7 @@ in {
   home.packages = with pkgs; [
     (google-cloud-sdk.withExtraComponents [google-cloud-sdk.components.gke-gcloud-auth-plugin])
     kubectl
+    k9s
 
     unstable.freelens-bin
   ];
